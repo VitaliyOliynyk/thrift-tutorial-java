2ecaaa7f6fd4e33ba4295e823ac226d4421b33cb
VitaliyOliynyk <xaoc00@gmail.com>
2014-07-24 00:22:29 +0200

    initial commit

---
+++ src/main/java/eu/vitaliy/thrifttutorial/App.java
@@ -0,0 +1,13 @@
+package eu.vitaliy.thrifttutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
